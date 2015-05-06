9c44af4a887087656cf72f563802e280cb407d34
Stephanie Marx <stephanie.n.marx@gmail.com>
2015-05-05 19:06:11 -0500

    Add figaro to properly manage api key and add simple request using api to index controller

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'wunderground'
+gem 'figaro'
 
 # Front end helpfulness
 gem 'bourbon'
--- app/controllers/api/v1/index_controller.rb
+++ app/controllers/api/v1/index_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::IndexController < ApplicationController
 	# authorize_resource class: false
 
 	def index
+		@w_api = Wunderground.new(Figaro.env.wunderground_api_key)
+		puts @w_api.forecast_for("WA", "Spokane")
 	end
 
 	def location
